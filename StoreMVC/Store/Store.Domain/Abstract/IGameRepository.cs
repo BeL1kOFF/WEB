ab3843ab9d26adbaa31af72f79d4e9d3d8270440
BeL1kOFF <ArtsiomBelikov@gmail.com>
2016-10-12 15:34:51 +0300

    route + Static repository

---
+++ StoreMVC/Store/Store.Domain/Abstract/IGameRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Store.Domain.Entities;
+
+namespace Store.Domain.Abstract
+{
+    public interface IGameRepository
+    {
+        IEnumerable<Game> Games { get; }
+    }
+}
--- StoreMVC/Store/WebUI/App_Start/RouteConfig.cs
+++ StoreMVC/Store/WebUI/App_Start/RouteConfig.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace WebUI
+namespace Store.WebUI
 {
     public class RouteConfig
     {
@@ -16,7 +16,7 @@ namespace WebUI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Game", action = "List", id = UrlParameter.Optional }
             );
         }
     }
